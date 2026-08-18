5bfbda605d4dc66e2416446aa81747b102135d82
gfogwill <german.perez-fogwill@fmi.fi>
2026-08-18 14:10:17 +0300

    refactor: use config.py addresses instead of address.py in matorova/main.py

--- payloads/matorova/main.py
+++ payloads/matorova/main.py
@@ -1,6 +1,6 @@
 import supervisor
 supervisor.runtime.autoreload = False
-import address
+
 import busio
 import config
 import logging
@@ -11,11 +11,13 @@ from sdcard import SDCard
 PAYLOAD_ID = "matorova"
 spi = busio.SPI(config.SPI_SCK, MOSI=config.SPI_MOSI, MISO=config.SPI_MISO)
 
+
 def main():
-    lora = LoRa(spi, address.matorova_rfm_address, address.ground_rfm_address)
+    lora = LoRa(spi, config.MATOROVA_RFM_ADDRESS, config.GROUND_RFM_ADDRESS)
     sd_card = SDCard(spi, PAYLOAD_ID)
     logger = logging.getLogger("{}-main".format(PAYLOAD_ID), sd_card)
     main_loop(lora=lora, payload_id=PAYLOAD_ID, logger=logger)
+
 
 if __name__ == "__main__":
     main()
